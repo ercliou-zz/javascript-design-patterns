7fcc78518422576efc09242e3e74e2cc534a06c3
Eric Liou <ercliou@gmail.com>
2015-02-15 16:45:06 -0200

    dual cat! semi-scalable

--- catclicker.js
+++ catclicker.js
@@ -1,15 +1,34 @@
 function registerCounter () {
 
-	var element = document.getElementById('clicky');
-	element.addEventListener('click', function() {
+	var firstCat = {
+		name: 'Grumpy',
+		element: document.getElementById('first-cat')
+	};
+	
+	var secondCat = {
+		name: 'Cute-cute',
+		element: document.getElementById('second-cat')
+	};
+
+	registerCat(firstCat);
+	registerCat(secondCat);
+
+}
+
+function registerCat (cat) {
+
+	var clicky = cat.element.getElementsByClassName('clicky')[0];
+
+	var nameEl = cat.element.getElementsByClassName('name')[0];
+	nameEl.innerText = cat.name;
+	
+	clicky.addEventListener('click', function() {
 		
-		var countElement = document.getElementById('count');
+		var countElement = cat.element.getElementsByClassName('count')[0];
 		var count = parseInt(countElement.innerText);
 		countElement.innerText = count + 1;
 
 	}, false);
-	
+
 }
-
-
 
