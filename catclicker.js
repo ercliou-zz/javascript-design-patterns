67599a29c3f0f74984b008da1819907851635a7f
Eric Liou <ercliou@gmail.com>
2015-02-15 16:27:17 -0200

    cat clicking with counter

--- catclicker.js
+++ catclicker.js
@@ -1,1 +1,15 @@
-// Hello
+function registerCounter () {
+
+	var element = document.getElementById('clicky');
+	element.addEventListener('click', function() {
+		
+		var countElement = document.getElementById('count');
+		var count = parseInt(countElement.innerText);
+		countElement.innerText = count + 1;
+
+	}, false);
+	
+}
+
+
+
