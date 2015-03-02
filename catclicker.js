d6dbc0dd803368d3971b60821c0e0f66c1ca29b6
Eric Liou <ercliou@gmail.com>
2015-03-01 23:31:45 -0300

    Cat Clicker Premium with Octopus and isolated vars :)

--- catclicker.js
+++ catclicker.js
@@ -1,69 +1,141 @@
-
-var catList = [
-	{
-		name: 'Grumpy',
-		image: 'grumpy.jpg',
-		count: 0
-	},
-	{
-		name: 'Cute-cute',
-		image: 'cute.jpg',
-		count: 0
-	},
-	{
-		name: 'Cups',
-		image: 'cups.jpg',
-		count: 0
-	}
-]
-
-
-function registerCounter () {
-
-	catList.forEach(function(el, i){
-		addToList(el);
-	});
-
-}
-
-function displayCat (cat) {
-
-	var displayEl = document.getElementById('cat-display');
-
-	// clean display
-	while (displayEl.firstChild) {
-    	displayEl.removeChild(displayEl.firstChild);
-	}
-
-	var title = document.createElement('h2');
-	title.innerText = cat.name;
-	displayEl.appendChild(title);
-
-	var img = document.createElement('img');
-	img.setAttribute('src', cat.image);
-	displayEl.appendChild(img);
-
-}
-
-function addToList (cat) {
-
-	var catListEl = document.getElementById('cat-list');
-
-	var elem = document.createElement('li');
-	
-
-	elem.textContent = cat.name;
-	elem.addEventListener('click', (function(cat) {
+var model = function() {
+	var catList;
+	return {
 		
-		return function(){
-			displayCat(cat);	
+		init: function (){
+			catList = [
+				{
+					name: 'Grumpy',
+					image: 'grumpy.jpg',
+					count: 0
+				},
+				{
+					name: 'Cute-cute',
+					image: 'cute.jpg',
+					count: 0
+				},
+				{
+					name: 'Cups',
+					image: 'cups.jpg',
+					count: 0
+				}
+			];
+		},
+	
+		getCatList: function() {
+			return catList;
 		}
 		
+		
+		
+	}
+}();
 
-	})(cat), false);
+var octopus = function(){
+	var selectedCat;
+	return {
+		init: function(){
+			model.init();
+			view.init();
+		},
+		
+		getCatList: function() {
+			return model.getCatList();
+		},
+		
+		selectCat: function(cat) {
+			selectedCat = cat;
+			view.displayCat(cat);
+			
+		},
+		
+		clickCat: function(cat) {
+			var cats = model.getCatList();
+			cat.count++;
+			view.displayCat(cat);
+		}
+	
+	}
+	
+	
+}();
+
+var view = function(){
+	
+	function addToList (cat) {
+
+		var catListEl = document.getElementById('cat-list');
+
+		var elem = document.createElement('li');
+		
+
+		elem.textContent = cat.name;
+		elem.addEventListener('click', (function(cat) {
+			
+			return function(){
+				octopus.selectCat(cat);	
+			}
+			
+
+		})(cat), false);
 
 
-	catListEl.appendChild(elem);
+		catListEl.appendChild(elem);
 
-}
+	}
+	
+	
+	return {
+		init: function(){
+			var cats = octopus.getCatList();
+			cats.forEach(function(el, i){
+				addToList(el);
+			});
+		
+		
+		},
+		
+		
+		displayCat: function (cat) {
+
+			var displayEl = document.getElementById('cat-display');
+
+			// clean display
+			while (displayEl.firstChild) {
+				displayEl.removeChild(displayEl.firstChild);
+			}
+
+			var title = document.createElement('h2');
+			title.innerText = cat.name;
+			displayEl.appendChild(title);
+			
+			var title = document.createElement('h3');
+			title.innerText = cat.count;
+			displayEl.appendChild(title);
+
+			
+			
+			
+			var img = document.createElement('img');
+			img.setAttribute('src', cat.image);
+			
+			img.addEventListener('click', (function(cat) {
+			
+				return function(){
+					octopus.clickCat(cat);	
+				}
+				
+
+			})(cat), false);
+			
+			displayEl.appendChild(img);
+
+		}
+	
+	}
+	
+}();
+
+
+
 
