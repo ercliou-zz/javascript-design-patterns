1a52ae1840c13fe15aba04015b66e0fa7bb9b471
Eric Liou <ercliou@gmail.com>
2015-03-15 23:13:26 -0300

    cat clicker premium PRO!

--- catclicker.js
+++ catclicker.js
@@ -32,11 +32,18 @@ var model = function() {
 }();
 
 var octopus = function(){
-	var selectedCat;
+	
+	
 	return {
 		init: function(){
+			
+			this.selectedCat;
+			this.doShowAdmin = false;
+			this.adminPanel = document.getElementById('admin-panel');
+			
 			model.init();
 			view.init();
+			adminView.init();
 		},
 		
 		getCatList: function() {
@@ -44,16 +51,52 @@ var octopus = function(){
 		},
 		
 		selectCat: function(cat) {
-			selectedCat = cat;
-			view.displayCat(cat);
+			this.selectedCat = cat;
+			view.displayCat();
 			
 		},
 		
+		getSelectedCat: function(){
+			return this.selectedCat;
+		},
+		
 		clickCat: function(cat) {
-			var cats = model.getCatList();
-			cat.count++;
-			view.displayCat(cat);
+			this.selectedCat.count++;
+			view.displayCat();
+		},
+		
+		showAdmin: function() {
+			this.doShowAdmin = true;
+			adminView.showAdmin();
+		},
+		
+		hideAdmin: function() {
+			this.doShowAdmin = false;
+			adminView.hideAdmin();
+		},
+		
+		isAdminShown: function() {
+			return this.doShowAdmin;
+		},
+		
+		toggleAdmin: function() {
+			if(octopus.isAdminShown()){
+				octopus.hideAdmin();
+			} else {
+				octopus.showAdmin();
+				
+			}
+		},
+		
+		save: function() {
+			this.selectedCat.name = document.getElementById('name').value;
+			this.selectedCat.image = document.getElementById('image-url').value;
+			this.selectedCat.count = document.getElementById('clicks').value;
+			
+			adminView.hideAdmin();
+			view.displayCat();
 		}
+		
 	
 	}
 	
@@ -63,25 +106,17 @@ var octopus = function(){
 var view = function(){
 	
 	function addToList (cat) {
-
 		var catListEl = document.getElementById('cat-list');
-
 		var elem = document.createElement('li');
-		
 
 		elem.textContent = cat.name;
 		elem.addEventListener('click', (function(cat) {
-			
 			return function(){
 				octopus.selectCat(cat);	
 			}
-			
-
 		})(cat), false);
 
-
 		catListEl.appendChild(elem);
-
 	}
 	
 	
@@ -91,13 +126,10 @@ var view = function(){
 			cats.forEach(function(el, i){
 				addToList(el);
 			});
-		
-		
 		},
 		
-		
-		displayCat: function (cat) {
-
+		displayCat: function () {
+			var cat = octopus.getSelectedCat();
 			var displayEl = document.getElementById('cat-display');
 
 			// clean display
@@ -111,10 +143,7 @@ var view = function(){
 			
 			var title = document.createElement('h3');
 			title.innerText = cat.count;
-			displayEl.appendChild(title);
-
-			
-			
+			displayEl.appendChild(title);			
 			
 			var img = document.createElement('img');
 			img.setAttribute('src', cat.image);
@@ -124,16 +153,60 @@ var view = function(){
 				return function(){
 					octopus.clickCat(cat);	
 				}
-				
 
 			})(cat), false);
 			
 			displayEl.appendChild(img);
-
 		}
-	
 	}
+}();
+
+var adminView = function(){
 	
+
+	return {
+		init: function(){
+			this.adminPanel = document.getElementById('admin-panel');
+			this.adminBtn = document.getElementById('admin-btn');
+			this.saveBtn = document.getElementById('save-btn');
+			this.cancelBtn = document.getElementById('cancel-btn');
+			
+			this.nameElm = document.getElementById('name');
+			this.imageUrlElm = document.getElementById('image-url');
+			this.clicksElm = document.getElementById('clicks');
+		
+		
+			octopus.hideAdmin();
+			this.adminBtn.onclick = function(){
+				octopus.toggleAdmin();
+			};
+			this.saveBtn.onclick = function(){
+				octopus.save();
+				octopus.hideAdmin();
+			};
+			this.cancelBtn.onclick = function(){
+				octopus.hideAdmin();
+				//nameElm.value =
+			};
+			
+		},
+		
+		showAdmin: function() {
+			this.adminPanel.style.visibility = 'visible';
+			var selectedCat = octopus.getSelectedCat();
+			this.nameElm.value = selectedCat.name;
+			this.imageUrlElm.value = selectedCat.image;
+			this.clicksElm.value = selectedCat.count;
+			
+		},
+		
+		hideAdmin: function() {
+			this.adminPanel.style.visibility = 'hidden';
+		},
+		
+		
+		
+	};
 }();
 
 
